1d1a87f02a82beaf7869bf4d82340649d0b38261
waamuscab <muscab9494@gmail.com>
2026-02-05 13:23:24 +0300

    Get values from separate inputs

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ document
   .getElementById("akanForm")
   .addEventListener("submit", function (event) {
     event.preventDefault();
-    console.log("event listener initiated");
+    // Get values from separate inputs
+    const day = parseInt(document.getElementById("day").value);
+    const month = parseInt(document.getElementById("month").value); // 0-indexed
+    const year = parseInt(document.getElementById("year").value);
+    const gender = document.querySelector('input[name="gender"]:checked');
+    console.log(day, month, year, gender);
   });
